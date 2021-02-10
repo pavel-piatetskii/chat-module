815279e03deea01aae7ca9bbf7e090f1f5894203
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-02-09 16:42:07 -0800

    Add user list and basic style for it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import UserList from "./UserList";
 import RoomList from "./RoomList";
 
 const data = {
-  users: {'1':{ id: 1, name: 'Jake' }, '2':{ id: 2, name: 'Jane' }},
+  users: {
+    '1': { id: 1, name: 'Jake', avatar: 'https://avatars.dicebear.com/4.5/api/gridy/1.svg' },
+    '2': { id: 2, name: 'Jane', avatar: 'https://avatars.dicebear.com/4.5/api/gridy/2.svg' }
+  },
   messages: [
     { id: 1, room: 1, sender: 1, message: 'Hi!' },
     { id: 2, room: 1, sender: 2, message: 'Hello! How\'r U?' },
@@ -16,7 +19,7 @@ function App() {
     <div className="App">
       <h1>Hello!</h1>
       <div className="main">
-        <UserList />
+        <UserList users={data.users}/>
         <MessageFeed />
         <RoomList />
       </div>
--- src/UserList.js
+++ src/UserList.js
@@ -4,7 +4,15 @@ import "./UserList.scss"
 export default function UserList(props) {
   return (
     <section className="user-list list-section">
-      <h2 className="user-list__header">This is the user list</h2>
+      <h2 className="user-list__header">Users in this room</h2>
+      <div className="user-list__users">
+        {Object.values(props.users).map((user) => (
+          <article className="user-list__users__user">
+            <img className="user-list__users__user__avatar" src={user.avatar}></img>
+            <mark className="user-list__users__user__name">{user.name}</mark>
+          </article>
+        )) }
+      </div>
     </section>
   )
 }

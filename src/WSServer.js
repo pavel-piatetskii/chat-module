24c2df2435f19c07c10eac226d05c5e0119727d9
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-03-09 15:12:22 -0800

    Refactor rooms into a single object

--- src/WSServer.js
+++ src/WSServer.js
@@ -1,70 +1,62 @@
 'use strict'
 const http = require('http');
-const { useReducer } = require('react');
 const WebSocket = require('ws');
 
 console.log("--- Creating Websocket servers for all Rooms");
-const wsServer3001 = new WebSocket.Server({ port: 3001 });
-const wsServer3002 = new WebSocket.Server({ port: 3002 });
-//const wsServers3001 = [new WebSocket.Server({ port: 3001 }), new WebSocket.Server({ port: 3002 })];
-console.log("***CREATED");
+const rooms = {
+  '1': {
+    server: new WebSocket.Server({ port: 3001 }),
+    port: 3001,
+    connections: [],
+    history: [],
+    users: [],
+  },
+  '2': {
+    server: new WebSocket.Server({ port: 3002 }),
+    port: 3002,
+    connections: [],
+    history: [],
+    users: [],
+  },
+}
 
-//let connections = [];
-//let history = [];
+for (const room in rooms) {
 
-let connections3001 = [];
-let history3001 = [];
-let users3001 = [];
+  const { server, port, connections, history, } = rooms[room];
+  let { users } = rooms[room];
 
-let connections3002 = [];
-let history3002 = [];
+  server.on('connection', ws => {
+    console.log(`New connection on port ${port}`)
 
-//let connections = { 'main': [], 'offtopic': [] };
-//let history = { 'main': [], 'offtopic': [] };
+    ws.send(JSON.stringify({ history }));
+    connections.push(ws);
 
-wsServer3001.on('connection', ws => {
-  console.log('New connection on port 3001')
-  
-  //console.log(Object.keys(ws['_events']))
+    ws.on('message', (message) => {
+      const { type, data } = JSON.parse(message);
+      console.log(`${type}: ${data}`);
 
-  ws.send(JSON.stringify({ history: history3001 }));
-  connections3001.push(ws);
-
-  ws.on('message', (message) => {
-    console.log('3001: ' + message);
-    const parsedMessage = (JSON.parse(message));
-    const { type, data } = parsedMessage;
-    console.log(`${type}: ${data}`);
-
-    switch (type) {
-      case 'newMessage':
-        history3001.push({ 
-          id: history3001.length,
-          data, time: new Date()
-        });
-        connections3001.map(ws => 
-          ws.send(JSON.stringify({ type, data }))
-        );
+      switch (type) {
+        case 'newMessage':
+          history.push({
+            id: history.length,
+            data, time: new Date()
+          });
+          connections.map(ws =>
+            ws.send(JSON.stringify({ type, data }))
+          );
           break;
-      case 'newUser':
-        users3001.push(data);
-        connections3001.map(ws => ws.send(JSON.stringify({ users: users3001 })));
-        break;
-      case 'userClosed':
-        users3001 = users3001.filter(user => user != data)
-        connections3001.map(ws => ws.send(JSON.stringify({ users: users3001 })));
-    }
+        case 'newUser':
+          users.push(data);
+          connections.map(ws => ws.send(JSON.stringify({ users })));
+          break;
+        case 'userClosed':
+          users = users.filter(user => user != data)
+          connections.map(ws => ws.send(JSON.stringify({ users })));
+      }
+    })
   })
-})
 
-wsServer3002.on('connection', ws => {
-  console.log('New connection on port 3002')
-  ws.send(JSON.stringify({ history: history3002 }));
-  connections3002.push(ws);
+  console.log("***ALL ROOMS CREATED");
 
-  ws.on('message', (message) => {
-    console.log('3002: ' + message);
-    history3002.push({ id: history3002.length, message, time: new Date() });
-    connections3002.map(ws => ws.send(message));
-  })
-})
+}
+console.log("***ALL ROOMS CREATED");
